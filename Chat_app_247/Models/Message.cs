592e1f9e9a8b3a9f48294f00f80a5a95eb7e9680
Dang Van Dang <makid1523@gmail.com>
2025-10-24 12:29:28 +0700

    Tạo schema mesage

---
+++ Chat_app_247/Models/Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_app_247.Models
+{
+    public class Message
+    {
+        // ID định danh duy nhất cho tin nhắn
+        public string MessageId { get; set; }
+
+        // ID của người gửi
+        public string SenderId { get; set; }
+
+        // Thời gian gửi tin nhắn (dùng kiểu long - Unix Timestamp để dễ sắp xếp)
+        public long Timestamp { get; set; }
+
+        // Loại tin nhắn: Text, Image, File...
+        public string MessageType { get; set; } // "Text", "Image", "File"
+
+        // Nội dung tin nhắn (nếu là MessageType.Text)
+        public string Content { get; set; }
+
+        // URL của file đính kèm (nếu là Image hoặc File)
+        public string FileUrl { get; set; }
+
+        // Tên file gốc để hiển thị cho người nhận
+        public string FileName { get; set; }
+    }
+}
