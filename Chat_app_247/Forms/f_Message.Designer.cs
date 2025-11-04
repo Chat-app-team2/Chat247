42f42619dccf620b7e5180d0b875ef7f06e929c3
van-767 <van767.1973@gmail.com>
2025-11-04 22:36:34 +0700

    Panel Message

--- Chat_app_247/Forms/f_Message.Designer.cs
+++ Chat_app_247/Forms/f_Message.Designer.cs
@@ -28,18 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Message_panel = new Panel();
+            Message_send_panel = new Panel();
             SuspendLayout();
+            // 
+            // Message_panel
+            // 
+            Message_panel.BorderStyle = BorderStyle.FixedSingle;
+            Message_panel.Dock = DockStyle.Left;
+            Message_panel.Location = new Point(0, 0);
+            Message_panel.Name = "Message_panel";
+            Message_panel.Size = new Size(257, 450);
+            Message_panel.TabIndex = 0;
+            // 
+            // Message_send_panel
+            // 
+            Message_send_panel.BorderStyle = BorderStyle.Fixed3D;
+            Message_send_panel.Dock = DockStyle.Fill;
+            Message_send_panel.Location = new Point(257, 0);
+            Message_send_panel.Name = "Message_send_panel";
+            Message_send_panel.Size = new Size(543, 450);
+            Message_send_panel.TabIndex = 1;
             // 
             // f_Message
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(Message_send_panel);
+            Controls.Add(Message_panel);
             Name = "f_Message";
             Text = "Message";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel Message_panel;
+        private Panel Message_send_panel;
     }
 }
