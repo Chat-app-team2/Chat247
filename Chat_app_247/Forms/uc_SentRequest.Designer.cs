297488eed3d0671eeaede776137f5add2c8ecc9a
Nguyen Ngoc Tuyen <nguyentuyen999999tn@gmail.com>
2025-11-15 17:32:50 +0700

    thêm chức năng và giao diện khi ấn xem thêm tại 1 số chỗ

--- Chat_app_247/Forms/uc_SentRequest.Designer.cs
+++ Chat_app_247/Forms/uc_SentRequest.Designer.cs
@@ -57,9 +57,10 @@
             uc_Sent_Panel.CustomizableEdges = customizableEdges6;
             uc_Sent_Panel.Dock = DockStyle.Fill;
             uc_Sent_Panel.Location = new Point(0, 0);
+            uc_Sent_Panel.Margin = new Padding(4);
             uc_Sent_Panel.Name = "uc_Sent_Panel";
             uc_Sent_Panel.ShadowDecoration.CustomizableEdges = customizableEdges7;
-            uc_Sent_Panel.Size = new Size(821, 123);
+            uc_Sent_Panel.Size = new Size(1232, 184);
             uc_Sent_Panel.TabIndex = 1;
             // 
             // Full_button
@@ -75,14 +76,14 @@
             Full_button.FillColor = SystemColors.MenuBar;
             Full_button.Font = new Font("Segoe UI", 9.857143F, FontStyle.Bold, GraphicsUnit.Point, 0);
             Full_button.ForeColor = Color.Black;
-            Full_button.Location = new Point(598, 31);
-            Full_button.Margin = new Padding(2);
+            Full_button.Location = new Point(1024, 46);
             Full_button.Name = "Full_button";
             Full_button.ShadowDecoration.CustomizableEdges = customizableEdges2;
             Full_button.ShadowDecoration.Enabled = true;
-            Full_button.Size = new Size(82, 56);
+            Full_button.Size = new Size(123, 84);
             Full_button.TabIndex = 16;
             Full_button.Text = "Xem Thêm";
+            Full_button.Click += Full_button_Click;
             // 
             // DeleteRequest_button
             // 
@@ -97,12 +98,11 @@
             DeleteRequest_button.FillColor = Color.FromArgb(231, 76, 80);
             DeleteRequest_button.Font = new Font("Segoe UI", 9.857143F, FontStyle.Bold, GraphicsUnit.Point, 0);
             DeleteRequest_button.ForeColor = Color.White;
-            DeleteRequest_button.Location = new Point(409, 31);
-            DeleteRequest_button.Margin = new Padding(2);
+            DeleteRequest_button.Location = new Point(668, 46);
             DeleteRequest_button.Name = "DeleteRequest_button";
             DeleteRequest_button.ShadowDecoration.CustomizableEdges = customizableEdges4;
             DeleteRequest_button.ShadowDecoration.Enabled = true;
-            DeleteRequest_button.Size = new Size(150, 56);
+            DeleteRequest_button.Size = new Size(225, 84);
             DeleteRequest_button.TabIndex = 15;
             DeleteRequest_button.Text = "Xóa Lời Mời";
             DeleteRequest_button.Click += DeleteRequest_button_Click;
@@ -112,9 +112,10 @@
             Name_Label.BackColor = Color.Transparent;
             Name_Label.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Name_Label.ForeColor = SystemColors.Desktop;
-            Name_Label.Location = new Point(113, 41);
+            Name_Label.Location = new Point(170, 62);
+            Name_Label.Margin = new Padding(4);
             Name_Label.Name = "Name_Label";
-            Name_Label.Size = new Size(75, 33);
+            Name_Label.Size = new Size(105, 46);
             Name_Label.TabIndex = 1;
             Name_Label.Text = "Name";
             // 
@@ -123,22 +124,24 @@
             Avartar_Picture.BackColor = Color.Transparent;
             Avartar_Picture.FillColor = Color.Black;
             Avartar_Picture.ImageRotate = 0F;
-            Avartar_Picture.Location = new Point(30, 27);
+            Avartar_Picture.Location = new Point(45, 40);
+            Avartar_Picture.Margin = new Padding(4);
             Avartar_Picture.Name = "Avartar_Picture";
             Avartar_Picture.ShadowDecoration.CustomizableEdges = customizableEdges5;
             Avartar_Picture.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            Avartar_Picture.Size = new Size(60, 60);
+            Avartar_Picture.Size = new Size(90, 90);
             Avartar_Picture.SizeMode = PictureBoxSizeMode.Zoom;
             Avartar_Picture.TabIndex = 0;
             Avartar_Picture.TabStop = false;
             // 
             // uc_SentRequest
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(uc_Sent_Panel);
+            Margin = new Padding(4);
             Name = "uc_SentRequest";
-            Size = new Size(821, 123);
+            Size = new Size(1232, 184);
             uc_Sent_Panel.ResumeLayout(false);
             uc_Sent_Panel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)Avartar_Picture).EndInit();
